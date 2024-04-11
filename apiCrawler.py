cd32233ff1d83fd015019aa9507c62afe8e4ad32
Steven Knapp <sknapp@Stevens-MacBook-Pro.local>
2024-04-11 18:25:23 -0400

    Updated apiCrawler.py to grab the abbreviated state data from state the user selected

--- apiCrawler.py
+++ apiCrawler.py
@@ -9,33 +9,61 @@ import pprint
 # API for TOC Contents - https://api.municode.com/codesToc/children?jobId=450310&nodeId=CH1GEPR&productId=14114
 # API for last child of TOC Contents - https://api.municode.com/CodesContent?jobId=450310&nodeId=CH1GEPR_S1-1HOCODECI&productId=14114
 
-
-
 pp = pprint.PrettyPrinter(indent=4)
+
+def getAPI(URL):
+  results = requests.get(URL)
+  return results
+
+# STEP 1 - Select state and get ${stateAbbr} from https://api.municode.com/States/
 URL = "https://api.municode.com/States/"
 headers = ""
-r = requests.get(URL)
+r = getAPI(URL)
 pp.pprint(r.json())
-# for index in r.json():
-#     print(index['StateName'])
+
 
 stateDict = {}
 for index in r.json():
-  stateDict[index['StateAbbreviation']] = index['StateName']
+  stateName = index['StateName']
+  stateAbbr = index['StateAbbreviation']
+  stateDict[stateAbbr] = stateName
+  # print(stateAbbr + " : " + stateName)
+pp.pprint(stateDict)
 
-# print(stateDict)
+
+for key in stateDict:
+  print ("the key name is " + key + " and its value is " + stateDict[key])
+
+
 questions = [
-  inquirer.List('state',
+  inquirer.List('states',
                 message="Please select a state to search:",
-               choices=stateDict.values(),)
-              # choices=["Virginia"],)
+              #  choices=stateDict.values(),)
+              choices=["Virginia"],)
 ]
 answers = inquirer.prompt(questions)
+print("You chose: ", answers['states'])
 
-# STEP 1 - Select state and get ${stateAbbr} from https://api.municode.com/States/
+stateAbbr = ''
+for key in stateDict:
+  if (stateDict[key] == answers['states']):
+    stateAbbr = key
+    break
+print("The abbreviation is: ", stateAbbr)
+
+
 # STEP 2 - Select county and get ${clientId} from https://api.municode.com/Clients/stateAbbr?stateAbbr=${stateAbbr}
-# STEP 3 - Get ${productId} from https://api.municode.com/ClientContent/${clientId}
-# STEP 4 - Get ${jobId} from https://api.municode.com/Jobs/latest/${productId}
-# STEP 5 - Get list of TOC children ${tocNodeId} from https://api.municode.com/codesToc?jobId=${jobId}&productId=${productId}
-# STEP 6 - Get list of Child Nodes ${childNodeId} from https://api.municode.com/codesToc/children?jobId=450310&nodeId=CH1GEPR&productId=14114
-# STEP 7 - Get text content of final child node from https://api.municode.com/CodesContent?jobId=${jobId}&nodeId=${nodeId}&productId=${productId}
+def getCounty(stateAbbr):
+  URL = f'https://api.municode.com/Clients/stateAbbr?stateAbbr={stateAbbr}'
+  print(URL)
+  r = requests.get(URL)
+  pp.pprint(r.json())
+
+getCounty(stateAbbr)
+   
+
+# STEP 3 - Get ${productId} from https://api.municode.com/ClientContent/{clientId}
+# STEP 4 - Get ${jobId} from https://api.municode.com/Jobs/latest/{productId}
+# STEP 5 - Get list of TOC children ${tocNodeId} from https://api.municode.com/codesToc?jobId={jobId}&productId={productId}
+# STEP 6 - Get list of Child Nodes ${childNodeId} from https://api.municode.com/codesToc/children?jobId={jobId}&nodeId={nodeId}&productId={productId}
+# STEP 7 - Get text content of final child node from https://api.municode.com/CodesContent?jobId={jobId}&nodeId={nodeId}&productId={productId}
